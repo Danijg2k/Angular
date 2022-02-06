150fa7581fa0dd24d5820e6f5852255b72b986c6
Danijg2k <danijg2000@gmail.com>
2022-02-06 20:15:20 +0100

    Estilos actualizados

--- base_project/src/app/components/items/items.component.ts
+++ base_project/src/app/components/items/items.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+// Needed for changing styles via css
+import { ViewEncapsulation } from '@angular/core'
+
 @Component({
+  encapsulation: ViewEncapsulation.None,
   selector: 'app-items',
   templateUrl: './items.component.html',
   styleUrls: ['./items.component.css']
 })
 export class ItemsComponent implements OnInit {
+
+
 
   constructor() { }
 
--- base_project/src/app/pages/index/index-component/index-component.component.ts
+++ base_project/src/app/pages/index/index-component/index-component.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+// Needed for changing styles via css
+import { ViewEncapsulation } from '@angular/core'
+
 @Component({
+  encapsulation: ViewEncapsulation.None,
   selector: 'app-index-component',
   templateUrl: './index-component.component.html',
   styleUrls: ['./index-component.component.css']
